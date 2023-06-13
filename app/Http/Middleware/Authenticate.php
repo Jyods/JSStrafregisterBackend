2f979c187bfdd519aa2342e915cbcbd4da15d3cd
Sprenger Jyodisen (JYO) <sprenger.jyodisen@suva.ch>
2023-06-13 15:09:28 +0200

    added unauthenticated

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class Authenticate extends Middleware
 {
@@ -13,5 +14,17 @@ class Authenticate extends Middleware
     protected function redirectTo(Request $request): ?string
     {
         return $request->expectsJson() ? null : route('login');
+    }
+
+    /**
+     * Handle an unauthenticated user.
+     *
+     * @param \Illuminate\Http\Request  $request
+     * @param array  $guards
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function unauthenticated($request, array $guards)
+    {
+        return new JsonResponse(['error' => 'Unauthenticated.'], 401);
     }
 }
