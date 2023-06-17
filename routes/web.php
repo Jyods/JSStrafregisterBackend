0603a88750d5062b1b4c86208bc37209b1c7fda7
Jyodisen <jyodisen_sprenger@sluz.ch>
2023-06-17 22:03:56 +0200

    removed login route

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('login', function () {
-    return response()->json(['message' => 'Please login'], 401);
-})->name('login');
