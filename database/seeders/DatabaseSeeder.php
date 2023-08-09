03d09d1c11b6a7da07fb1dd5bdda791babd6bc65
Jyods <jyodi.sprenger@hotmail.ch>
2023-08-09 21:28:33 +0200

    added test laws

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -27,6 +27,116 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $laws = [
+            (object) [
+                "Title" => "Beleidigung",
+                "Category" => "1",
+                "Severity" => "High",
+                "ShortDescription" => "Ein Angriff auf die Person durch verbale Worten oder Taten wird als Beleidigung gezählt.",
+                "Description" => "Die Aussagen einer Person werden nach Ermessen der Exekutive behandelt.",
+                "minJail" => 0,
+                "maxJail" => 10
+            ],
+            (object) [
+                "Title" => "Respektlosigkeit",
+                "Category" => "1",
+                "Severity" => "High",
+                "ShortDescription" => "Eine Respektlosigkeit oder Untergraben des Respekts einer Person kann von der Exekutive als Beleidigung deklariert werden.",
+                "Description" => "Die Maximale Strafe beläuft sich auf: 10 Hafteinheiten",
+                "minJail" => 0,
+                "maxJail" => 10
+            ],
+            (object) [
+                "Title" => "Leichter Diebstahl",
+                "Category" => "1",
+                "Severity" => "Medium",
+                "ShortDescription" => "Ein leichter Diebstahl beläuft sich auf ein sehr geringes Entwenden einer nicht wichtigen Ressource.",
+                "Description" => "Die Maximale Strafe beläuft sich auf: 10 Hafteinheiten",
+                "minJail" => 0,
+                "maxJail" => 10
+            ],
+            (object) [
+                "Title" => "Undiszipliniertes Verhalten",
+                "Category" => "1",
+                "Severity" => "High",
+                "ShortDescription" => "Als Undiszipliniertes Verhalten zählt unter anderem die Respektlosigkeit gegenüber Nieder- / Gleich- und Höherrangigen oder ein falsches Verhalten zeigen.",
+                "Description" => "Die Maximale Strafe beläuft sich auf: Zur Grundausbildung auf Kamino schicken oder Haftzeit: 10 Hafteinheiten",
+                "minJail" => 0,
+                "maxJail" => 10
+            ],
+            (object) [
+                "Title" => "Verstoß gegen Sicherheitslevel",
+                "Category" => "1",
+                "Severity" => "High",
+                "ShortDescription" => "Ein Verstoß gegen das Sicherheitslevel bezeichnet das Betreten einer Anlage bzw. eines Bereiches mit einem höheren Sicherheitslevel als das Eigene.",
+                "Description" => "Die Maximale Strafe beläuft sich auf: 15 Hafteinheiten",
+                "minJail" => 0,
+                "maxJail" => 15
+            ],
+            (object) [
+                "Title" => "Entziehung vor Sanktionen",
+                "Category" => "2",
+                "Severity" => "High",
+                "ShortDescription" => "Wer sich seiner Sanktion entzieht, als Beispiel: Bußgeld wissentlich nicht bezahlen oder das Shuttle nach Kamino trotz Auflage nicht nehmen, macht sich strafbar.",
+                "Description" => "Die Maximale Strafe beläuft sich auf: 20 Hafteinheiten",
+                "minJail" => 0,
+                "maxJail" => 20
+            ],
+            (object) [
+                "Title" => "Schwerer Diebstahl",
+                "Category" => "2",
+                "Severity" => "High",
+                "ShortDescription" => "Schwerer Diebstahl bezeichnet das Entwenden von großen, vielen und/oder wichtigen Ressourcen.",
+                "Description" => "Beispiel: Das Entwenden eines schweren Ionen Disruptors aus der Waffenkammer.\nDie Maximale Strafe beläuft sich auf: 20 Hafteinheiten",
+                "minJail" => 0,
+                "maxJail" => 20
+            ],
+            (object) [
+                "Title" => "Belästigung",
+                "Category" => "2",
+                "Severity" => "High",
+                "ShortDescription" => "Das Belästigen von Personen oder Personengruppen kann auch als undiszipliniertes Verhalten angesehen werden.",
+                "Description" => "Als Belästigung wird das stören oder nerven von Personen genannt.\nDie Maximale Strafe beläuft sich auf: 20 Hafteinheiten",
+                "minJail" => 0,
+                "maxJail" => 20
+            ],
+            (object) [
+                "Title" => "Verstoß der Start- / Landeerlaubnis Richtlinien",
+                "Category" => "2",
+                "Severity" => "High",
+                "ShortDescription" => "Die Richtlinie besagt dass: “Zu startende oder landende Personen müssen ihr Anliegen erst anfragen bevor sie zum Start bzw. zur Landung ansetzen.",
+                "Description" => "Dabei wird ihnen ein Flugplatz zugewiesen, den sie benutzen können. Sollte keine solche Anfrage erfolgen, können Konsequenzen je nach Schweregrad bis zur Zerstörung des Fahrzeuges folgen.\nDie Maximale Strafe beläuft sich auf: 30 Hafteinheiten",
+                "minJail" => 0,
+                "maxJail" => 30
+            ],
+            (object) [
+                "Title" => "Unbefugtes Benutzen einer Ressource",
+                "Category" => "2",
+                "Severity" => "High",
+                "ShortDescription" => "Das unbefugte benutzen einer Ressource bezeichnet das benutzen einer Ressource wie z.B. das benutzen eines AT-TE ohne Erlaubnis oder Ausbildung.",
+                "Description" => "Die Maximale Strafe beläuft sich auf: 25 Hafteinheiten, Entzug aus jeglichen Fortbildungen für Fahrzeuge",
+                "minJail" => 0,
+                "maxJail" => 25
+            ],
+            (object) [
+                "Title" => "Widerstand gegen die Exekutive",
+                "Category" => "2",
+                "Severity" => "High",
+                "ShortDescription" => "Als Widerstand gegen die Exekutive ist das Nichtbefolgen der Anweisungen der Exekutiven Kraft oder das Aktive zur Wehr setzen gegen natürliche oder juristische Personen der Exekutiven Kraft gemeint.",
+                "Description" => "Die Maximale Strafe beläuft sich auf: 20 Hafteinheiten",
+                "minJail" => 0,
+                "maxJail" => 20
+            ],
+            (object) [
+                "Title" => "Behinderung der Notfall Kräfte",
+                "Category" => "2",
+                "Severity" => "High",
+                "ShortDescription" => "Notfall Kräfte, die aktiv oder passiv von Personen behindert werden, können Maßnahmen gegebenenfalls verzögern.",
+                "Description" => "Der Fall stellt eine Straftat dar.\nDie Maximale Strafe beläuft sich auf: 20 Hafteinheiten",
+                "minJail" => 0,
+                "maxJail" => 20
+            ],
+        ];
 
         //erstelle eine Variable mit einem Array voller Rang Objekten
         $ranks = [
@@ -256,7 +366,19 @@ class DatabaseSeeder extends Seeder
         ->count(50)
         ->create();
 
-        Law::factory(20)->create();
+        foreach ($laws as $law) {
+            Law::factory()->create([
+                'Title' => $law->Title,
+                'Category' => $law->Category,
+                'Severity' => $law->Severity,
+                'ShortDescription' => $law->ShortDescription,
+                'Description' => $law->Description,
+                'minJail' => $law->minJail,
+                'maxJail' => $law->maxJail,
+            ]);
+        }
+
+        //Law::factory(20)->create();
 
         FileLaw::factory()
         ->count(100)
